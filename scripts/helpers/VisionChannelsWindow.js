be4a30ccd0ae323ed6281c27c5ef83e506d05f92
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2023-11-19 23:43:04 +0100

    settings window

--- scripts/helpers/VisionChannelsWindow.js
+++ scripts/helpers/VisionChannelsWindow.js
@@ -2,6 +2,18 @@ import { cModuleName, Translate } from "../utils/PerceptiveUtils.js";
 
 const cWindowID = "vision-channels-window";
 const cConfirmIcon = "fa-solid fa-check";
+const cAddIcon = "fa-solid fa-plus";
+const cSettingName = "VisionChannels";
+
+const cDefaultChannel = {
+	Name : "Vision Channel",
+	Color : "#ffffff",
+	RequiredtoSee : false,
+	SeethroughWalls : false,
+	Range : -1,
+	EffectFilter : null,
+	Transparency : 1
+};
 
 class VisionChannelsWindow extends Application {
 	constructor(pOptions = {}) {
@@ -25,37 +37,41 @@ class VisionChannelsWindow extends Application {
 	}
 	
 	getHTML(pOptions={}) {
+		//randomID()
+		let vChannels = game.settings.get(cModuleName, cSettingName);
+		
 		let vEntriesHTML = `<table>`;
 		
-		vEntriesHTML = `<tr>
-							<th>${Translate(cWindowID + ".entries.titles." + "name")}</th>
-							<th>${Translate(cWindowID + ".entries.titles." + "required")}</th>
-							<th>${Translate(cWindowID + ".entries.titles." + "throughwalls")}</th>
-							<th>${Translate(cWindowID + ".entries.titles." + "range")}</th>
-							<th>${Translate(cWindowID + ".entries.titles." + "color")}</th>
-							<th>${Translate(cWindowID + ".entries.titles." + "filter")}</th>
-							<th>${Translate(cWindowID + ".entries.titles." + "transparency")}</th>
-						</tr>`;
+		vEntriesHTML = vEntriesHTML+ 	`<tr style="border: 1px solid #dddddd">
+											<th style="border: 1px solid #dddddd">${Translate(cWindowID + ".entries.titles." + "name")}</th>
+											<th style="border: 1px solid #dddddd">${Translate(cWindowID + ".entries.titles." + "required")}</th>
+											<th style="border: 1px solid #dddddd">${Translate(cWindowID + ".entries.titles." + "throughwalls")}</th>
+											<th style="border: 1px solid #dddddd">${Translate(cWindowID + ".entries.titles." + "range")}</th>
+											<th style="border: 1px solid #dddddd">${Translate(cWindowID + ".entries.titles." + "color")}</th>
+											<th style="border: 1px solid #dddddd">${Translate(cWindowID + ".entries.titles." + "filter")}</th>
+											<th style="border: 1px solid #dddddd">${Translate(cWindowID + ".entries.titles." + "transparency")}</th>
+										</tr>`;
 		
 		for (let i = 0; i < 1; i++) {
 			vEntriesHTML = vEntriesHTML + 	`	<tr>
 													<td> <input type="text"> </td>
-													<td> <input type="checkbox"> </td>
-													<td> <input type="checkbox"> </td>
-													<td> <input type="number"> </td>
-													<td> <input type="color"> </td>
+													<td style="text-align: center; width:100px"> <input type="checkbox"> </td>
+													<td style="text-align: center; width:100px"> <input type="checkbox"> </td>
+													<td style="width:50px"> <input type="number"> </td>
+													<td style="text-align: center; width:50px"> <input type="color" style=""> </td>
 													<td> 
 														<select> 
 														</select>
 													</td>
-													<td> <input type="number"> </td>
+													<td style="width:50px"> <input type="number"> </td>
 												</tr>`;
 		}
 		
 		vEntriesHTML = vEntriesHTML + `</table>`;
 		
 		//buttons	
-		let vButtonsHTML = 				`<div class="form-group" style="display:flex;flex-direction:row;align-items:center;gap:1em;margin-top:1em">
+		let vButtonsHTML = 				`<div class="form-group" style="display:flex;flex-direction:column;align-items:center;gap:1em;margin-top:1em">
+											<button type="button" name="${cWindowID}.addchannel"> <i class="${cAddIcon}"></i> ${Translate(cWindowID + ".buttons.add.name")} </button>
 											<button type="button" name="${cWindowID}.confirmchanges"> <i class="${cConfirmIcon}"></i> ${Translate(cWindowID + ".buttons.confirm.name")} </button>
 										</div>`;
 										
@@ -87,6 +103,15 @@ function testwindow() {
 	new VisionChannelsWindow().render(true);
 }
 
-Hooks.once("ready", function() {
-	game.modules.get("perceptive").api.test = {testwindow}
+Hooks.once("init", function() {
+	game.settings.register(cModuleName, cSettingName, {
+		name: Translate("Settings." + cSettingName + ".name"),
+		hint: Translate("Settings." + cSettingName + ".descrp"),
+		scope: "world",
+		config: false,
+		type: Object,
+		default: {"000" : cDefaultChannel}
+	});
+	
+	game.modules.get("perceptive").api.test = {testwindow};
 });
